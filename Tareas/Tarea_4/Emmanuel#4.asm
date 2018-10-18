e5e26a6e06eacd661fbdb3af5db96099763d7c36
Emmanuel <ema2159@gmail.com>
2018-10-17 23:28:10 -0600

    Probados LEDs. El teclado parece funcionar. Se necesita corregir

--- Tareas/Tarea_4/Emmanuel#4.asm
+++ Tareas/Tarea_4/Emmanuel#4.asm
@@ -1,9 +1,24 @@
 ;***********************************************************************
-															;Tarea 4
+;                             Tarea 4
 ;Autor:Emmanuel Bustos T.
 ;Descripción: Este programa consiste en leer e interpretar las entradas 
 ;brindadas en la tarjeta de training Dragon 12 por el teclado matricial, 
-;implementando las subrutinas necesarias.
+;implementando las subrutinas necesarias.                                                                     
+;                 -----------------                                                     
+;                 |   |   |   |   |                                                     
+;                 | 1 | 2 | 3 | 4 |                                                     
+;            PA4--------------------                                           
+;                 |   |   |   |   |                                           
+;                 | 5 | 6 | 7 | 8 |                                           
+;            PA5--------------------                                           
+;                 |   |   |   |   |                                           
+;                 | B | 9 | 0 | E |                                           
+;            PA6--------------------                                           
+;                 |   |   |   |   |                                                   
+;                 | NA| NA| NA| NA|                                                   
+;            PA7-------------------- 
+;                     |   |   |   |
+;                    PA0 PA1 PA2 PA3
 ;***********************************************************************
 #include registers.inc
 					ORG $3E70
@@ -30,11 +45,14 @@ TECLAS:		DB $01,$02,$03,$04,$05,$06,$07,$08,$0B,$09,$0E
 					MOVB #$31,RTICTL
 					BSET CRGINT,$80
 					;Configurar LEDS
-					MOVB DDRB,$FF
+					MOVB #$FF,DDRB
 					BSET DDRJ,$02
 					BCLR PTJ,$02
+					MOVB #$0F,DDRP
 					;Configurar la mitad de los bits de teclado como entrada y la otra mitad como salida
-					MOVB DDRA,$0F
+					MOVB #$F0,DDRA
+					;Habilitar resistencias de pullup
+				;	BSET PUCR,$01
 					;Habilitar instrucciones mascarables
 					CLI
 					;Configurar stack
@@ -42,23 +60,27 @@ TECLAS:		DB $01,$02,$03,$04,$05,$06,$07,$08,$0B,$09,$0E
 					BRA *
 
 RTI_ISR:	TST REB
-					BEQ DEC_REB
+					LBEQ DEC_REB
 					MOVB	#$FF,BUFFER
 ;*********LEER TECLA***************
 					MOVB #0,PATRON
-					LDAA #EF
+					LDAA #$EF
 					LDX TECLAS
 LOOP_TEC:	LDAB PATRON
 					CMPB #4
 					BEQ FIN_LEER
 					STAA PORTA
 					LDAB #0
+					MOVB PORTA,PORTB
 					BRCLR PORTA,$01,ENC_TEC
 					INCB
+					MOVB PORTA,PORTB
 					BRCLR PORTA,$02,ENC_TEC
 					INCB
+					MOVB PORTA,PORTB
 					BRCLR PORTA,$04,ENC_TEC
 					INCB
+					MOVB PORTA,PORTB
 					BRCLR PORTA,$08,ENC_TEC
 					INC PATRON
 					ROLA
@@ -67,28 +89,28 @@ ENC_TEC:	LSL PATRON
 					LSL PATRON
 					ADDB PATRON
 					MOVB B,X BUFFER
+				;	MOVB BUFFER,PORTB
 FIN_LEER:
 ;**********************************					
-					LDAA #FF
+					LDAA #$FF
 					CMPA BUFFER
 					BEQ TEC_NE
-					TST PRIMERA
-					BNE TEC_NE
+					BRSET BANDERAS,$03,TEC_NE
 					BSET BANDERAS,$04
 					MOVB #10,REB
 					MOVB BUFFER,TECLA
 					BRA RTI_RTRN
-TEC_NE:		CMPA #FF
+TEC_NE:		CMPA #$FF
 					BEQ RTI_RTRN
 					BRSET BANDERAS,$02,IS_VALID
 					CMPA BUFFER
 					BEQ	TEC_BUFF
 					MOVB #$FF,TECLA
 					BRA RTI_RTRN
-TEC_BUFF:	BSET BANDERAS, $02
+TEC_BUFF:	BSET BANDERAS,$02
 					BRA RTI_RTRN
 IS_VALID: LDAB BUFFER
-					CMPB #FF
+					CMPB #$FF
 					BNE	RTI_RTRN
 					BSET BANDERAS,$01
 					BCLR BANDERAS,$06
